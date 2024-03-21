34e45212d15400ef7ed0309a5dacacb5e7777760
Darius Daniel <dariusdaniel771@gmail.com>
2024-03-21 18:19:01 +0100

    Built the main body of the trial page

---
+++ src/components/LinkTable.tsx
@@ -0,0 +1,95 @@
+import './styles/LinkTable.css';
+
+export default function LinkTable() {
+  const tableHeaders: Array<string> = [
+    'Short Link',
+    'Original Link',
+    'Clicks',
+    'Date',
+  ];
+  const testData: Array<{
+    short: string;
+    long: string;
+    clicks: number;
+    date: string;
+  }> = [
+    {
+      short: 'https://linkly.com/Bn41aCOlnxj',
+      long: 'https://www.twitter.com/tweets/8erelCoihu/',
+      clicks: 1313,
+      date: 'Oct-10-2023',
+    },
+    {
+      short: 'https://linkly.com/Bn41aCOlnxj',
+      long: 'https://www.twitter.com/tweets/8erelCoihu/',
+      clicks: 1313,
+      date: 'Oct-10-2023',
+    },
+    {
+      short: 'https://linkly.com/Bn41aCOlnxj',
+      long: 'https://www.twitter.com/tweets/8erelCoihu/',
+      clicks: 1313,
+      date: 'Oct-10-2023',
+    },
+    {
+      short: 'https://linkly.com/Bn41aCOlnxj',
+      long: 'https://www.twitter.com/tweets/8erelCoihu/',
+      clicks: 1313,
+      date: 'Oct-10-2023',
+    },
+    {
+      short: 'https://linkly.com/Bn41aCOlnxj',
+      long: 'https://www.twitter.com/tweets/8erelCoihu/',
+      clicks: 1313,
+      date: 'Oct-10-2023',
+    },
+    {
+      short: 'https://linkly.com/Bn41aCOlnxj',
+      long: 'https://www.twitter.com/tweets/8erelCoihu/',
+      clicks: 1313,
+      date: 'Oct-10-2023',
+    },
+    {
+      short: 'https://linkly.com/Bn41aCOlnxj',
+      long: 'https://www.twitter.com/tweets/8erelCoihu/',
+      clicks: 1313,
+      date: 'Oct-10-2023',
+    },
+    {
+      short: 'https://linkly.com/Bn41aCOlnxj',
+      long: 'https://www.twitter.com/tweets/8erelCoihu/',
+      clicks: 1313,
+      date: 'Oct-10-2023',
+    },
+    {
+      short: 'https://linkly.com/Bn41aCOlnxj',
+      long: 'https://www.twitter.com/tweets/8erelCoihu/',
+      clicks: 1313,
+      date: 'Oct-10-2023',
+    },
+  ];
+  return (
+    <table className="link-table">
+      <thead>
+        {tableHeaders.map((colHead, idx) => (
+          <th scope="col" key={idx} className="text-lite">
+            {colHead}
+          </th>
+        ))}
+      </thead>
+      <tbody>
+        {testData.map((data, idx) => {
+          const { short, long, clicks, date } = data;
+          return (
+            <tr key={idx} className="text-lite">
+              <td>{short}</td>
+              <td>{long}</td>
+              <td>{clicks}</td>
+              <td>{date}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+}
