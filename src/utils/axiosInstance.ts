77bade65c7cdd801ee7b84867d37df91f6306cd4
Darius Daniel <dariusdaniel771@gmail.com>
2024-04-17 15:25:03 +0100

    Create production branch

--- src/utils/axiosInstance.ts
+++ src/utils/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from 'axios';
 
 const axiosInstance: AxiosInstance = axios.create({
-  baseURL: 'http://127.0.0.1:5000',
+  baseURL: 'https://linkly-1xxj.onrender.com:5000',
   headers: { 'Content-Type': 'application/json' }
 });
 
