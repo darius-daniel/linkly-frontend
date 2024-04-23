64b1057a42a7563e8c31a1631b159332b9e70075
Darius Daniel <dariusdaniel771@gmail.com>
2024-04-23 17:49:35 +0100

    Wrapped the favicon in a span

--- src/components/LinkTable.tsx
+++ src/components/LinkTable.tsx
@@ -60,16 +60,18 @@ export default function LinkTable({
                 </a>
               </td>
               <td>
-                <img
-                  alt="site favicon"
-                  src={getFavicon(longUrl)}
-                  style={{
-                    display: 'inline-block',
-                    height: '48px',
-                    width: '48px',
-                    margin: '5px',
-                  }}
-                ></img>
+                <span>
+                  <img
+                    alt="site favicon"
+                    src={getFavicon(longUrl)}
+                    style={{
+                      display: 'inline-block',
+                      height: '48px',
+                      width: '48px',
+                      margin: '5px',
+                    }}
+                  ></img>
+                </span>
                 {longUrl.slice(0, 25)}...
               </td>
               <td>{clicks}</td>
